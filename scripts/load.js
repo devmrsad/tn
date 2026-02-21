a8a64df6cf9d23f933bd29c6bd32ba177d9db8b5
Mohammadreza <devmrsad@gmail.com>
2026-02-21 22:00:42 +0330

    improved UX -> loader deleted and smooth scroll removed

--- scripts/load.js
+++ scripts/load.js
@@ -1,11 +1,11 @@
 window.scrollTo(0, 0)
 
-const protector = document.querySelector(".protector")
-window.addEventListener("load", () => {
-    setTimeout(() => {
-        protector.classList.remove("protector-active")
-        setTimeout(() => {
-            protector.style.display = "none"
-        }, 320);
-    }, 250);
-})
+// const protector = document.querySelector(".protector")
+// window.addEventListener("load", () => {
+//     setTimeout(() => {
+//         protector.classList.remove("protector-active")
+//         setTimeout(() => {
+//             protector.style.display = "none"
+//         }, 320);
+//     }, 250);
+// })
